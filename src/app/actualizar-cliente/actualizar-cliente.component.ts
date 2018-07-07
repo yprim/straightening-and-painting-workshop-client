c14643d4e496f0188bc33de7996a96a9cf049efb
yprim <priscipo@gmail.com>
2018-07-07 16:40:11 -0600

    vista con enrutamiento¨s

---
+++ src/app/actualizar-cliente/actualizar-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-cliente',
+  templateUrl: './actualizar-cliente.component.html',
+  styleUrls: ['./actualizar-cliente.component.css']
+})
+export class ActualizarClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrarClienteComponent } from './registrar-cliente/registrar-cliente.component';
+import { ActualizarClienteComponent } from './actualizar-cliente/actualizar-cliente.component';
+import { OrdenTrabajoComponent } from './orden-trabajo/orden-trabajo.component';
+import { RegistrarVehiculoComponent } from './registrar-vehiculo/registrar-vehiculo.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReportesComponent } from './reportes/reportes.component';
+
+const appRoutes: Routes =[
+{ path: 'actualizar-cliente', component: ActualizarClienteComponent },
+{ path: 'orden-trabajo', component: OrdenTrabajoComponent },
+{ path: 'registrar-cliente', component: RegistrarClienteComponent },
+{ path: 'registrar-vehiculo', component: RegistrarVehiculoComponent },
+{ path: 'reportes', component: ReportesComponent },
+{ path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: '**', redirectTo: '/home', pathMatch: 'full' }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarClienteComponent,
+    ActualizarClienteComponent,
+    OrdenTrabajoComponent,
+    RegistrarVehiculoComponent,
+    ReportesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+		HttpClientModule,
+		HttpModule,
+		FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
